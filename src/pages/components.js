6585a8c59174be6575070c29d26770bef45dba39
Rune Madsen <rune@runemadsen.com>
2021-08-30 14:51:00 -0400

    small fixes

--- src/pages/components.js
+++ src/pages/components.js
@@ -3,21 +3,17 @@ import Layout from "../components/Layout";
 import Heading from "../components/Heading";
 import Breadcrumb from "../components/Breadcrumbs";
 
-const breadcrumbs = [
-  { name: "Videos Overview", link: "" },
-  { name: "Tracks", link: "" },
-  { name: "The Nature of Code", link: "" },
-];
-
 const ComponentsPage = () => {
   return (
     <Layout>
       <Heading>This is a heading</Heading>
-      <Heading>This is a heading</Heading>
-      <Heading>This is a heading</Heading>
-      <Breadcrumb breadcrumbs={breadcrumbs}>This is a Breadcrumb</Breadcrumb>
-      <h2>This is a subheading</h2>
-      <p>Hello</p>
+      <Breadcrumb
+        breadcrumbs={[
+          { name: "Videos Overview", link: "" },
+          { name: "Tracks", link: "" },
+          { name: "The Nature of Code", link: "" },
+        ]}
+      />
     </Layout>
   );
 };
