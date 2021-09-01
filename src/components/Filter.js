2f71c9c1743b77ca7baa0bca70917e3594139109
Rune Madsen <rune@runemadsen.com>
2021-09-01 15:16:05 -0400

    more filter

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,18 +1,49 @@
-import React, { memo } from "react";
-import cn from "classnames";
+import React, { memo } from 'react';
+import cn from 'classnames';
 
-import * as css from "./Filter.module.css";
-import { box } from "../styles/box.module.css";
+import * as css from './Filter.module.css';
+import { box } from '../styles/box.module.css';
 
-const Filter = ({ title, className, seeMore = "See more" }) => {
+const Filter = ({
+  title,
+  items = [],
+  selected = [],
+  seeMore = 'See more',
+  onChange,
+  className
+}) => {
+  const onClick = (item) => {
+    let newSelected = selected.slice();
+    const idx = selected.indexOf(item);
+
+    if (idx > -1) {
+      newSelected.splice(idx, 1);
+    } else {
+      newSelected.push(item);
+    }
+
+    onChange(newSelected);
+  };
+
   return (
     <div className={cn(css.root, className)}>
       <div className={cn(box, css.left)}>
         <div className={cn(box, css.icon)}>Ⴤ</div>
       </div>
-      <div className={cn(box, css.right)}>
+      <div className={css.right}>
         <div className={cn(box, css.title)}>
           <h3>{title}</h3>
+        </div>
+        <div className={css.items}>
+          {items.map((item) => (
+            <div
+              key={item}
+              className={cn(box, css.item, {
+                [css.selected]: selected.includes(item)
+              })}>
+              <button onClick={() => onClick(item)}>{item}</button>
+            </div>
+          ))}
         </div>
       </div>
     </div>
