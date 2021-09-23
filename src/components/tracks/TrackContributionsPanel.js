c83e67d44e5de684d126a92540bbb7194ba2845f
Anadroid <anajelicic89@gmail.com>
2021-09-23 17:15:18 +0200

    Initial structure for contributions

--- src/components/tracks/TrackContributionsPanel.js
+++ src/components/tracks/TrackContributionsPanel.js
@@ -1,9 +1,57 @@
 import React, { memo } from 'react';
+import ButtonPanel from '../ButtonPanel';
 
 import * as css from './TrackContributionsPanel.module.css';
 
+import PlayButton from '../../images/playbutton.svg';
+
+//dummy content
+const contributions = [
+  {
+    title: "Grab any spring particle",
+    author: "galiton"
+  },
+  {
+    title: "Grab any spring particle",
+    author: "galiton"
+  },
+  {
+    title: "Grab any spring particle",
+    author: "galiton"
+  },
+  {
+    title: "Grab any spring particle",
+    author: "galiton"
+  }
+];
+
 const TrackContributionsPanel = ({ video }) => {
-  return <div className={css.root}></div>;
+  return (
+    <div className={css.root}>
+      <div className={css.titleBox}>
+        <h4>Comunity contributions</h4>
+        <p>What our community has done based on this video</p>
+      </div>
+      <div className={css.contributions}>
+        {contributions.map((contrib, key) => (
+          <div className={css.contrib}>
+            <span>{contrib.title}</span>
+            <p>
+            <span>{contrib.author}</span>
+            <PlayButton />
+            </p>
+          </div>
+        ))}
+      </div>
+      <ButtonPanel
+        text={'Have you completed a project? Share your work!'}
+        buttonText={'Submit a contribution'}
+        buttonLink={''}
+        variant={'purple'}
+        className={css.contribsPanel}
+      />
+    </div>
+  );
 };
 
 export default memo(TrackContributionsPanel);
