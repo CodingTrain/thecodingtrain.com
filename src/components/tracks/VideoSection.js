6dc009e143f5bb3df127f00b0cb43c772830ea4b
Fernando Florenzano Hernandez <f.florenzano94@gmail.com>
2021-11-02 13:25:06 -0300

    Fixed some cases in details tabs

--- src/components/tracks/VideoSection.js
+++ src/components/tracks/VideoSection.js
@@ -24,6 +24,12 @@ const VideoSection = ({ track, video, trackPosition }) => {
     setShowTimeline(false);
   }, []);
 
+  let labels = ['OVERVIEW'];
+  if (video.codeExamples && video.codeExamples.length > 0) {
+    labels.push('CODE EXAMPLES');
+  }
+  labels = [...labels, ...video.groupLinks.map((g) => g.title.toUpperCase())];
+
   return (
     <div className={css.root}>
       <div className={css.subheading}>
@@ -88,14 +94,7 @@ const VideoSection = ({ track, video, trackPosition }) => {
       </div>
       <div className={css.sep}></div>
       <div>
-        <Tabs
-          className={css.aboutTabs}
-          variant="red"
-          labels={[
-            'OVERVIEW',
-            'CODE EXAMPLES',
-            ...video.groupLinks.map((g) => g.title.toUpperCase())
-          ]}>
+        <Tabs className={css.aboutTabs} variant="red" labels={labels}>
           <CollapsableDescription
             className={css.description}
             expandedClassName={css.descriptionExpanded}
@@ -103,10 +102,11 @@ const VideoSection = ({ track, video, trackPosition }) => {
             content={video.description}
             charLimit={150}
           />
-          <div>
-            <CodeExampleList examples={video.codeExamples} variant="red" />
-          </div>
-
+          {video.codeExamples && video.codeExamples.length > 0 && (
+            <div>
+              <CodeExampleList examples={video.codeExamples} variant="red" />
+            </div>
+          )}
           {video.groupLinks.map((g, index) => (
             <LinkList links={g.links} variant="red" key={index} />
           ))}
