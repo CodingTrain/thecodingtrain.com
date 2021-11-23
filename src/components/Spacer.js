ff9ae95454db1855e5f570b58071a2af559293e8
Martin Bravo <bravomartin@gmail.com>
2021-11-23 13:08:52 -0300

    order imports alphabetically makes things worse

--- src/components/Spacer.js
+++ src/components/Spacer.js
@@ -2,6 +2,7 @@ import React, { memo } from 'react';
 import cn from 'classnames';
 
 import * as css from './Spacer.module.css';
+
 import { pattern as patternClass } from '../styles/styles.module.css';
 
 const Spacer = ({ pattern, children }) => {
