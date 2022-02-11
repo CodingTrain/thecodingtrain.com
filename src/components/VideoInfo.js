4ed36465a93483902be4abd2a072d326061960c9
Martin Bravo <bravomartin@gmail.com>
2022-02-11 19:11:28 -0300

    added sample content; changed order of tabs

--- src/components/VideoInfo.js
+++ src/components/VideoInfo.js
@@ -8,15 +8,22 @@ import CollapsableDescription from './CollapsableDescription';
 import * as css from './VideoInfo.module.css';
 
 const VideoInfo = ({ video, variant }) => {
-  let labels = ['OVERVIEW'];
+  let labels = ['DESCRIPTION'];
+
   if (video.codeExamples && video.codeExamples.length > 0) {
-    labels.push('CODE EXAMPLES');
+    labels.unshift('CODE EXAMPLES');
   }
+
   labels = [...labels, ...video.groupLinks.map((g) => g.title.toUpperCase())];
 
   return (
     <div className={cn(css.root, { [css[variant]]: variant })}>
       <Tabs className={css.aboutTabs} variant={variant} labels={labels}>
+        {video.codeExamples && video.codeExamples.length > 0 && (
+          <div>
+            <CodeExampleList examples={video.codeExamples} variant={variant} />
+          </div>
+        )}
         <CollapsableDescription
           className={css.description}
           expandedClassName={css.descriptionExpanded}
@@ -24,11 +31,6 @@ const VideoInfo = ({ video, variant }) => {
           content={video.description}
           charLimit={150}
         />
-        {video.codeExamples && video.codeExamples.length > 0 && (
-          <div>
-            <CodeExampleList examples={video.codeExamples} variant={variant} />
-          </div>
-        )}
         {video.groupLinks.map((g, index) => (
           <LinkList links={g.links} variant={variant} key={index} />
         ))}
