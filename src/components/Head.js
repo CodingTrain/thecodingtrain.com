1670ffc7b3a6cedce58b7c76245d2ee3b593e2db
Fernando Florenzano Hernandez <f.florenzano94@gmail.com>
2022-03-02 11:51:12 -0300

    :star: Added description to site

--- src/components/Head.js
+++ src/components/Head.js
@@ -12,6 +12,10 @@ const Head = ({ title }) => {
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width,initial-scale=1" />
       <title>{title}</title>
+      <meta
+        name="description"
+        content="All aboard the Coding Train with Daniel Shiffman, a YouTube channel dedicated to beginner-friendly creative coding tutorials and challenges."
+      />
     </Helmet>
   );
 };
