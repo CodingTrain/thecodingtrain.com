327fc53935564b9d16584d962a684b218c49c6f7
Fernando Florenzano Hernandez <f.florenzano94@gmail.com>
2022-03-03 12:13:19 -0300

    :star: Adds current date in topbar

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -7,7 +7,39 @@ import * as css from './TopBar.module.css';
 
 import Logo from '../images/logo.svg';
 
+const suffix = (day) => {
+  switch (day[day.length - 1]) {
+    case '1':
+      return 'st';
+    case '2':
+      return 'nd';
+    case '3':
+      return 'rd';
+    default:
+      return 'th';
+  }
+};
+const shortDate = (date) => date.toLocaleDateString('en-US');
+const longDate = (date) => {
+  const month = date.toLocaleDateString('en-US', {
+    month: 'short'
+  });
+  const year = date.toLocaleDateString('en-US', {
+    year: 'numeric'
+  });
+  const weekday = date.toLocaleDateString('en-US', {
+    weekday: 'long'
+  });
+  const day = date.toLocaleDateString('en-US', {
+    day: 'numeric'
+  });
+  const daySuffix = suffix(day);
+  return `${weekday} / ${month} ${day}${daySuffix}, ${year}`;
+};
+
 const TopBar = () => {
+  const today = new Date();
+  console.log(longDate(today));
   return (
     <div className={css.outer}>
       <div className={css.root}>
@@ -18,8 +50,8 @@ const TopBar = () => {
         </div>
         <div className={css.clock}>🕛</div>
         <div className={css.date}>
-          <span className={css.longDate}>Sunday / Feb 7th, 2021</span>
-          <span className={css.shortDate}>09/07/2021</span>
+          <span className={css.longDate}>{longDate(today)}</span>
+          <span className={css.shortDate}>{shortDate(today)}</span>
         </div>
         <Menu />
       </div>
