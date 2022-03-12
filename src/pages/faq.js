cf758f50de521a98e61913db7b350c1657e3952c
Fernando Florenzano Hernandez <f.florenzano94@gmail.com>
2022-03-12 12:18:41 -0300

    :star: Added character spacers in FAQs

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -6,9 +6,13 @@ import { Heading1 } from '../components/Heading';
 import Spacer from '../components/Spacer';
 import Question from '../components/Question';
 
-import CurlyBracketsCharacter from '../images/characters/CurlyBrackets_3.mini.svg';
+import BracketsCharacter1 from '../images/characters/CurlyBrackets_3.mini.svg';
+import BracketsCharacter2 from '../images/characters/CurlyBrackets_2.mini.svg';
+import BracketsCharacter3 from '../images/characters/CurlyBrackets_1.mini.svg';
+import BracketsCharacter4 from '../images/characters/CurlyBrackets_4.mini.svg';
 
 import * as css from '../styles/pages/faq.module.css';
+import CharacterSpacer from '../components/CharacterSpacer';
 
 const FAQPage = ({ data }) => {
   const { sections } = data.order.nodes[0];
@@ -20,7 +24,7 @@ const FAQPage = ({ data }) => {
           FAQ
         </Heading1>
         <div className={css.character}>
-          <CurlyBracketsCharacter />
+          <BracketsCharacter1 />
         </div>
       </div>
       <div className={css.description}>
@@ -31,6 +35,15 @@ const FAQPage = ({ data }) => {
           aliquip ex ea commodo consequat.
         </p>
       </div>
+      <CharacterSpacer
+        className={css.sep}
+        variant="pink"
+        size="x2"
+        side="left"
+        offset={0.7}
+        characterSize={1.2}
+        Character={BracketsCharacter2}
+      />
       <div className={css.sections}>
         {sections.map((section, sectionIndex) => (
           <Fragment key={sectionIndex}>
@@ -40,7 +53,19 @@ const FAQPage = ({ data }) => {
                 <Question key={index} variant="pink" {...question} />
               ))}
             </div>
-            <Spacer />
+            <CharacterSpacer
+              className={css.sep}
+              variant="pink"
+              size={sectionIndex !== sections.length - 1 ? 'x3' : 'x4'}
+              side="right"
+              offset={0.4}
+              characterSize={sectionIndex !== sections.length - 1 ? 1 : 1.2}
+              Character={
+                sectionIndex !== sections.length - 1
+                  ? BracketsCharacter3
+                  : BracketsCharacter4
+              }
+            />
           </Fragment>
         ))}
       </div>
