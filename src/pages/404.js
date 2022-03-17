4056c42b6280d33a5777484f50509464e32dbb39
Fernando Florenzano Hernandez <f.florenzano94@gmail.com>
2022-03-17 11:08:05 -0300

    :star: Basic layout, mobile and characters missing

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,53 +1,42 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
 
-// styles
-const pageStyles = {
-  color: '#232129',
-  padding: '96px',
-  fontFamily: '-apple-system, Roboto, sans-serif, serif'
-};
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320
-};
+import Layout from '../components/Layout';
+import CharacterSpacer from '../components/CharacterSpacer';
+import { Heading1 } from '../components/Heading';
+import ButtonPanel from '../components/ButtonPanel';
+import Spacer from '../components/Spacer';
 
-const paragraphStyles = {
-  marginBottom: 48
-};
-const codeStyles = {
-  color: '#8A6534',
-  padding: 4,
-  backgroundColor: '#FFF4DB',
-  fontSize: '1.25rem',
-  borderRadius: 4
-};
+import SemiColonCharacter from '../images/characters/SemiColon_4.mini.svg';
+
+import * as css from '../styles/pages/404.module.css';
 
 // markup
 const NotFoundPage = () => {
   return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{' '}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{' '}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === 'development' ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
+    <Layout title="Page not found!">
+      <Spacer />
+      <div className={css.header}>
+        <Heading1 className={css.heading} variant="pink" as="h2">
+          Page not found!
+        </Heading1>
+        <div className={css.character}>
+          <SemiColonCharacter />
+        </div>
+      </div>
+      {/* <CharacterSpacer
+        className="charSpacer"
+        variant="pink"
+        size="x3"
+        Character={SemiColonCharacter}
+      /> */}
+      <ButtonPanel
+        text="Oops! Sorry we couldn’t find what you were looking for."
+        buttonText="Go home"
+        buttonLink="/"
+        variant="pink"
+      />
+      <Spacer pattern />
+    </Layout>
   );
 };
 
