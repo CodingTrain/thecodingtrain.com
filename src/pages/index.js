f62227ed09c0f87e935719c079b8b83ed91fee14
Fernando Florenzano Hernandez <f.florenzano94@gmail.com>
2022-03-21 10:31:26 -0300

    new to coding base

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,10 @@ import { Link } from 'gatsby';
 
 import Layout from '../components/Layout';
 import { Heading1, Heading2 } from '../components/Heading';
+import ButtonPanel from '../components/ButtonPanel';
 import Spacer from '../components/Spacer';
 
+import Train from '../images/train.svg';
 import TriangleCharacter from '../images/characters/Triangle_6.mini.svg';
 import BracketsCharacter from '../images/characters/SquareBrackets_2.mini.svg';
 import SquareCharacter from '../images/characters/Square_4.mini.svg';
@@ -24,6 +26,12 @@ const IndexPage = () => {
           <Heading1 className={css.heading} variant="pink">
             All aboard!
           </Heading1>
+          <p>
+            Welcome to the Coding Train with Daniel Shiffman! A YouTube channel
+            dedicated to learning creative coding through beginner-friendly
+            projects.
+          </p>
+          <Train className={css.train} />
         </div>
         <Spacer pattern size="x2" />
         <div className={css.subheader}>
@@ -32,6 +40,26 @@ const IndexPage = () => {
           </Heading2>
           <div className={css.character}>
             <TriangleCharacter />
+          </div>
+        </div>
+        <div className={css.newToCoding}>
+          <div className={css.left}>
+            <p>
+              All aboard the Coding Train with Daniel Shiffman, a YouTube
+              channel dedicated to beginner-friendly.
+            </p>
+          </div>
+          <div className={css.right}>
+            <ButtonPanel
+              variant="orange"
+              text="We’ve created a simple guide to help you get started"
+              buttonText="GUIDE THE RIDE"
+            />
+            <ButtonPanel
+              variant="orange"
+              text="have questions? our community is here to help"
+              buttonText="join discord"
+            />
           </div>
         </div>
         <Spacer pattern size="x2" />
@@ -65,14 +93,10 @@ const IndexPage = () => {
             <SemiColonCharacter />
           </div>
         </div>
-        <Spacer pattern size="x2" />
-        <Heading2 className={css.subheading} variant="orange" as="h1">
-          Shop
-        </Heading2>
 
         <Spacer pattern size="x2" />
-        <Heading2 className={css.subheading} variant="pink" as="h3">
-          Become a member
+        <Heading2 className={css.subheading} variant="orange" as="h3">
+          SUPPORT
         </Heading2>
         <Spacer pattern size="x2" />
       </div>
