142f692d0b00b2d6e0f82de22adcc0a36977017a
fdoflorenzano <f.florenzano94@gmail.com>
2022-06-08 21:08:37 -0400

    feat: added basic instance of meta images and other meta tags

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,24 +1,63 @@
 import React, { memo } from 'react';
 import { Helmet } from 'react-helmet';
+import { useStaticQuery, graphql } from 'gatsby';
+
+const URL = 'https://thecodingtrain.com/';
+const defaultTitle = 'The Coding Train';
+const defaultDescription =
+  'All aboard the Coding Train with Daniel Shiffman, a YouTube channel dedicated to beginner-friendly creative coding tutorials and challenges.';
 
 const Head = ({ title, description }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      coverImage: allFile(
+        filter: {
+          name: { eq: "placeholder" }
+          sourceInstanceName: { eq: "main-tracks" }
+          extension: { in: ["jpg", "png"] }
+        }
+      ) {
+        nodes {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
+      }
+    }
+  `);
+  const image =
+    data.coverImage.nodes[0].childImageSharp.gatsbyImageData.images.fallback
+      .src;
+  // console.log({ image });
   return (
     <Helmet
       htmlAttributes={{
         lang: 'en'
       }}
-      defaultTitle="The Coding Train"
-      titleTemplate="%s / The Coding Train">
+      defaultTitle={defaultTitle}
+      titleTemplate={`%s / ${defaultTitle}`}>
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width,initial-scale=1" />
       <title>{title}</title>
+      <meta name="description" content={description ?? defaultDescription} />
+
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={URL} />
+      <meta property="og:title" content={title ?? defaultTitle} />
       <meta
-        name="description"
-        content={
-          description ??
-          'All aboard the Coding Train with Daniel Shiffman, a YouTube channel dedicated to beginner-friendly creative coding tutorials and challenges.'
-        }
+        property="og:description"
+        content={description ?? defaultDescription}
       />
+      <meta property="og:image" content={image} />
+
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content={URL} />
+      <meta property="twitter:title" content={title ?? defaultTitle} />
+      <meta
+        property="twitter:description"
+        content={description ?? defaultDescription}
+      />
+      <meta property="twitter:image" content={image} />
     </Helmet>
   );
 };
