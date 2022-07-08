7a24bc20514f7c785708c78ccaab74806ceaf286
Kathy <65121394+kfahn22@users.noreply.github.com>
2022-07-08 11:05:38 -0400

    Update src/components/PassengerShowcaseForm.js
    Co-authored-by: Daniel Shiffman <daniel.shiffman@gmail.com>

--- src/components/PassengerShowcaseForm.js
+++ src/components/PassengerShowcaseForm.js
@@ -190,7 +190,7 @@ const PassengerShowcaseForm = () => {
           <span>
             A JPG or PNG image to be shown in the passenger showcase. The
             image should be maximum 800 pixels wide and 500 kb in size and should visually represent your
-            output of your code.  The image will be displayed with a 16:9 aspect ratio.
+            work.  The image will be displayed with a 16:9 aspect ratio.
           </span>
         </label>
         <label>
