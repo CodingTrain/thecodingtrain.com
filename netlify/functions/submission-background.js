51db5dd3c7c2267e93651173d7ffc57ea478d844
klinegareth <klinegareth@gmail.com>
2022-08-30 16:51:54 -0400

    Changed indentation and title

--- netlify/functions/submission-background.js
+++ netlify/functions/submission-background.js
@@ -110,12 +110,11 @@ exports.handler = async function (event) {
     Make a PR to main
   **/
   const prRes = await octokit.request(`POST /repos/${owner}/${repo}/pulls`, {
-    title: `Passenger Showcase contribution from ${postInfo.authorName}`,
+    title: `Showcase Submission for ${postInfo.challenge}`,
     body: `Thank you ${postInfo.authorName} for your contribution! A member of the Coding Train team will review it shortly.
 
-
-	* [${postInfo.title}](${postInfo.url})
-	* [${postInfo.authorName}](${postInfo.authorUrl})`,
+* [${postInfo.title}](${postInfo.url})
+* [${postInfo.authorName}](${postInfo.authorUrl})`,
     head: branchName,
     base: 'main'
   });
