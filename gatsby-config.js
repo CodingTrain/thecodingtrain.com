d6696452e9fe4a81069d885004cca45972e5094b
Francis Turmel <fturmel@gmail.com>
2022-12-06 21:01:05 -0500

    downgrade a few deps

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     description:
       'All aboard the Coding Train with Daniel Shiffman, a YouTube channel dedicated to beginner-friendly creative coding tutorials and challenges.'
   },
+  trailingSlash: 'never',
   plugins: [
     {
       resolve: `gatsby-plugin-postcss`,
@@ -17,11 +18,11 @@ module.exports = {
           require('postcss-normalize'),
           require('postcss-nesting'),
           require('postcss-custom-properties')({
-            // importFrom: './src/styles/variables.css'
+            importFrom: './src/styles/variables.css'
           }),
           require('postcss-calc')(),
           require('postcss-custom-media')({
-            // importFrom: './src/styles/variables.css'
+            importFrom: './src/styles/variables.css'
           })
         ]
       }
@@ -41,16 +42,14 @@ module.exports = {
       options: {
         name: 'images',
         path: './src/images/'
-      },
-      __key: 'images'
+      }
     },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'pages',
         path: './src/pages/'
-      },
-      __key: 'pages'
+      }
     },
     {
       resolve: 'gatsby-source-filesystem',
@@ -58,21 +57,17 @@ module.exports = {
         name: 'videos',
         path: './content/videos',
         ignore: [
-          './**/src',
-          './**/videos/challenges/**/*',
-          './**/videos/guest-tutorials/**/*'
+          './**/videos/challenges/**/*'
+          // './**/videos/guest-tutorials/**/*'
         ]
-      },
-      __key: 'videos'
+      }
     },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'challenges',
-        path: './content/videos/challenges',
-        ignore: [`./**/src`]
-      },
-      __key: 'challenges'
+        path: './content/videos/challenges'
+      }
     },
     // {
     //   resolve: 'gatsby-source-filesystem',
@@ -81,31 +76,27 @@ module.exports = {
     //     path: './content/videos/guest-tutorials',
     //     ignore: [`./**/src`]
     //   },
-    //   __key: 'guest-tutorials'
     // },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'main-tracks',
         path: './content/tracks/main-tracks'
-      },
-      __key: 'main-tracks'
+      }
     },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'side-tracks',
         path: './content/tracks/side-tracks'
-      },
-      __key: 'side-tracks'
+      }
     },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'guides',
         path: './content/pages/guides'
-      },
-      __key: 'guides'
+      }
     },
     {
       resolve: `gatsby-plugin-mdx`,
@@ -121,48 +112,42 @@ module.exports = {
       options: {
         name: 'homepage-data',
         path: './content/pages/homepage'
-      },
-      __key: 'homepage-data'
+      }
     },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'tracks-page-data',
         path: './content/pages/tracks'
-      },
-      __key: 'tracks-page-data'
+      }
     },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'challenges-page-data',
         path: './content/pages/challenges'
-      },
-      __key: 'challenges-page-data'
+      }
     },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'faqs',
         path: './content/pages/faqs'
-      },
-      __key: 'faqs'
+      }
     },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'about-page-data',
         path: './content/pages/about'
-      },
-      __key: 'about-page-data'
+      }
     },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: '404-page-data',
         path: './content/pages/404'
-      },
-      __key: '404-page-data'
+      }
     },
     {
       resolve: 'gatsby-plugin-react-svg',
