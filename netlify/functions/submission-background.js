0ca1392e0a06d93d4fab9d15c6307a46e94c9861
Dipam Sen <funplanet.m4f@gmail.com>
2023-04-26 17:35:29 +0000

    use github database api to create blob first

--- netlify/functions/submission-background.js
+++ netlify/functions/submission-background.js
@@ -5,12 +5,15 @@ const sharp = require('sharp');
 // event.body expected to be:
 // {
 //   title: "Something",
+//   imageBlobSha: "sha",
 //   authorName: "Coding Train",
 //   authorUrl: "https://thecodingtrain.com",
+//   authorEmail: "help@thecodingtrain.com",
+//   authorTwitter: "@thecodingtrain",
+//   authorInstagram: "@the.coding.train"
 //   url: "https://thecodingtrain.com/tracks",
 //   challenge: "01-test",
-//   imageUrl: "https://github.com/branch/image.png",
-//   branchName: "showcase-coding-train-1234567890"
+//   imageExtension: "png|jpg"
 // }
 
 exports.handler = async function (event) {
@@ -25,13 +28,119 @@ exports.handler = async function (event) {
   const postInfo = JSON.parse(event.body);
 
   // Shared properties
+  const unix = Math.floor(Date.now() / 1000);
   const owner = 'CodingTrain';
   const repo = 'thecodingtrain.com';
+  const showcasePath = `content/videos/challenges/${postInfo.challenge}/showcase`;
+  const jsonPath = `${showcasePath}/contribution-${unix}.json`;
+  const imagePath = `${showcasePath}/contribution-${unix}.${postInfo.imageExtension}`;
   const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
+
+  /**
+    Get the SHA of the main branch
+  **/
+  const shaRes = await octokit.request(
+    `GET /repos/${owner}/${repo}/git/ref/heads/main`
+  );
+  const mainSha = shaRes.data.object.sha;
+
+  /**
+    Add the JSON file
+  **/
+  const json = {
+    title: postInfo.title,
+    author: {
+      name: postInfo.authorName
+    },
+    url: postInfo.url,
+    submittedOn: new Date().toISOString()
+  };
+
+  if (postInfo.authorUrl) {
+    json.author.url = postInfo.authorUrl;
+  }
+
+  // Including the social media handles in the JSON for future reference
+  if (postInfo.authorTwitter) {
+    json.author.twitter = postInfo.authorTwitter;
+  }
+
+  if (postInfo.authorInstagram) {
+    json.author.instagram = postInfo.authorInstagram;
+  }
+
+  const jsonString = JSON.stringify(json, null, 2) + '\n';
+  const jsonContent = Buffer.from(jsonString).toString('base64');
+
+  const jsonBlobRes = await octokit.request(
+    `POST /repos/${owner}/${repo}/git/blobs`,
+    {
+      content: jsonContent,
+      encoding: 'base64'
+    }
+  );
+
+  /**
+    Create a git tree
+   */
+  const treeRes = await octokit.request(
+    `POST /repos/${owner}/${repo}/git/trees`,
+    {
+      tree: [
+        {
+          path: jsonPath,
+          mode: '100644',
+          type: 'blob',
+          sha: jsonBlobRes.data.sha
+        },
+        {
+          path: imagePath,
+          mode: '100644',
+          type: 'blob',
+          sha: postInfo.imageBlobSha
+        }
+      ],
+      base_tree: mainSha
+    }
+  );
+
+  const commitOpts = {
+    message: 'Added new contribution',
+    tree: treeRes.data.sha,
+    parents: [mainSha]
+  };
+
+  if (postInfo.authorName && postInfo.authorEmail) {
+    commitOpts.author = {
+      name: postInfo.authorName,
+      email: postInfo.authorEmail
+    };
+  }
+
+  const commitRes = await octokit.request(
+    `POST /repos/${owner}/${repo}/git/commits`,
+    commitOpts
+  );
+
+  /**
+    Make a new branch
+  **/
+  const branchName = slugify(
+    `showcase-${slugify(postInfo.authorName)}-${unix}`.toLowerCase()
+  );
+
+  const branchRes = await octokit.request(
+    `POST /repos/${owner}/${repo}/git/refs`,
+    {
+      ref: `refs/heads/${branchName}`,
+      sha: commitRes.data.sha
+    }
+  );
 
   /**
     Make a PR to main
   **/
+  const imgUrl = `https://raw.githubusercontent.com/${owner}/${repo}/${commitRes.data.sha}/${imagePath}`;
   const prRes = await octokit.request(`POST /repos/${owner}/${repo}/pulls`, {
     title: `Showcase Submission for ${postInfo.challenge}`,
     body: `Thank you ${
@@ -45,8 +154,8 @@ exports.handler = async function (event) {
         : postInfo.authorName
     }
 
-![preview image](${postInfo.imageUrl})`,
-    head: postInfo.branchName,
+![preview image](${imgUrl})`,
+    head: branchName,
     base: 'main'
   });
 
--- netlify/functions/submission-sync.js
+++ netlify/functions/submission-sync.js
@@ -1,5 +1,4 @@
 const { Octokit } = require('@octokit/core');
-const slugify = require('slugify');
 const sharp = require('sharp');
 
 // event.body expected to be:
@@ -43,7 +42,7 @@ exports.handler = async function (event, context) {
     console.error('GitHub Token not loaded');
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: 'GitHub Token not loaded' })
+      body: JSON.stringify({})
     };
   }
 
@@ -61,102 +60,19 @@ exports.handler = async function (event, context) {
   }
 
   // Shared properties
-  const unix = Math.floor(Date.now() / 1000);
   const owner = 'CodingTrain';
   const repo = 'thecodingtrain.com';
-  const showcasePath = `content/videos/challenges/${postInfo.challenge}/showcase`;
-  const jsonPath = `${showcasePath}/contribution-${unix}.json`;
-  const imagePath = `${showcasePath}/contribution-${unix}.${imageExtension}`;
   const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
 
   /**
-    Get the SHA of the main branch
-  **/
-  const shaRes = await octokit.request(
-    `GET /repos/${owner}/${repo}/git/ref/heads/main`
-  );
-  const mainSha = shaRes.data.object.sha;
-
-  /**
-    Make a new branch
-  **/
-  const branchName = slugify(
-    `showcase-${slugify(postInfo.authorName)}-${unix}`.toLowerCase()
-  );
-
-  const branchRes = await octokit.request(
-    `POST /repos/${owner}/${repo}/git/refs`,
+    Create the image blob
+   */
+  const blobRes = await octokit.request(
+    `POST /repos/${owner}/${repo}/git/blobs`,
     {
-      ref: `refs/heads/${branchName}`,
-      sha: mainSha
+      content: postInfo.image,
+      encoding: 'base64'
     }
-  );
-
-  /**
-    Add the JSON file
-  **/
-  const json = {
-    title: postInfo.title,
-    author: {
-      name: postInfo.authorName
-    },
-    url: postInfo.url,
-    submittedOn: new Date().toISOString()
-  };
-
-  if (postInfo.authorUrl) {
-    json.author.url = postInfo.authorUrl;
-  }
-
-  // Including the social media handles in the JSON for future reference
-  if (postInfo.authorTwitter) {
-    json.author.twitter = postInfo.authorTwitter;
-  }
-
-  if (postInfo.authorInstagram) {
-    json.author.instagram = postInfo.authorInstagram;
-  }
-
-  const jsonString = JSON.stringify(json, null, 2) + '\n';
-  const jsonContent = Buffer.from(jsonString).toString('base64');
-
-  const jsonOpts = {
-    branch: branchName,
-    message: 'Added contribution JSON file',
-    content: jsonContent
-  };
-
-  if (postInfo.authorName && postInfo.authorEmail) {
-    jsonOpts.committer = {
-      name: postInfo.authorName,
-      email: postInfo.authorEmail
-    };
-  }
-
-  const jsonRes = await octokit.request(
-    `PUT /repos/${owner}/${repo}/contents/${jsonPath}`,
-    jsonOpts
-  );
-
-  /**
-    Add the image
-  **/
-  const imageOpts = {
-    branch: branchName,
-    message: 'Added contribution image file',
-    content: postInfo.image
-  };
-
-  if (postInfo.authorName && postInfo.authorEmail) {
-    imageOpts.committer = {
-      name: postInfo.authorName,
-      email: postInfo.authorEmail
-    };
-  }
-
-  const imageRes = await octokit.request(
-    `PUT /repos/${owner}/${repo}/contents/${imagePath}`,
-    imageOpts
   );
 
   /**
@@ -170,10 +86,13 @@ exports.handler = async function (event, context) {
         title: postInfo.title,
         authorName: postInfo.authorName,
         authorUrl: postInfo.authorUrl,
+        authorEmail: postInfo.authorEmail,
+        authorTwitter: postInfo.authorTwitter,
+        authorInstagram: postInfo.authorInstagram,
         url: postInfo.url,
         challenge: postInfo.challenge,
-        imageUrl: imageRes.data.content.download_url,
-        branchName: branchName
+        imageBlobSha: blobRes.data.sha,
+        imageExtension
       }),
       headers: {
         'Content-Type': 'application/json'
@@ -184,8 +103,7 @@ exports.handler = async function (event, context) {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      status: 'success',
-      branchName: branchName
+      status: 'success'
     })
   };
 };
