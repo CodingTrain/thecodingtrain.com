d7bca49c6008785e5a7ae9079bab31b97f87104b
Daniel Shiffman <daniel.shiffman@gmail.com>
2026-04-23 18:17:07 -0400

    Fix for Error 153 YouTube embeds (#1976)
    * discord invite update
    
    * adding strict-origin-when-cross-origin according to new YT API docs

--- src/components/Head.js
+++ src/components/Head.js
@@ -49,6 +49,7 @@ const Head = ({ title, description, image }) => {
       titleTemplate={`%s / ${defaultTitle}`}>
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width,initial-scale=1" />
+      <meta name="referrer" content="strict-origin-when-cross-origin" />
       <title>{title}</title>
       <meta name="description" content={description ?? defaultDescription} />
 
